af3fde3b0b5d9594e6b8ec4d27c823046c007dd0
Nursultan Adiev <adiev_n@mail.ru>
2025-02-09 15:17:30 +0600

    Implement User Authentication

--- flaskblog/__init__.py
+++ flaskblog/__init__.py
@@ -1,13 +1,17 @@
+import os
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
-import os
-
+from flask_bcrypt import Bcrypt
+from flask_login import LoginManager
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = 'e040592789b71d4d66fa87839be22de9'
-
 basedir = os.path.abspath(os.path.dirname(__file__))  # Get the current directory
 app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{os.path.join(basedir, "site.db")}'
+app.config['SECRET_KEY'] = 'e040592789b71d4d66fa87839be22de9'
 db = SQLAlchemy(app)
+bcrypt = Bcrypt(app)
+login_manager = LoginManager(app)
+login_manager.login_view = 'login'
+login_manager.login_message_category = 'info'
 
 from flaskblog import routes
