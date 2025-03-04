d6f594b636e654816e99aa145cef8a4d8893d51b
Nursultan Adiev <adiev_n@mail.ru>
2025-03-04 11:00:28 +0600

    Implement email and password reset

--- flaskblog/__init__.py
+++ flaskblog/__init__.py
@@ -3,15 +3,27 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
 from flask_login import LoginManager
+from flask_mail import Mail
+from dotenv import load_dotenv
+
+load_dotenv()
 
 app = Flask(__name__)
 basedir = os.path.abspath(os.path.dirname(__file__))  # Get the current directory
-app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{os.path.join(basedir, "site.db")}'
-app.config['SECRET_KEY'] = 'e040592789b71d4d66fa87839be22de9'
+
+app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{os.path.join(basedir, "site.db")}' # Set the database URI
+app.config['SECRET_KEY'] = 'e040592789b71d4d66fa87839be22de9' # Set the secret key
 db = SQLAlchemy(app)
 bcrypt = Bcrypt(app)
 login_manager = LoginManager(app)
 login_manager.login_view = 'login'
 login_manager.login_message_category = 'info'
+
+app.config['MAIL_SERVER'] = 'smtp.mail.ru'
+app.config['MAIL_PORT'] = 465
+app.config['MAIL_USE_SSL'] = True
+app.config['MAIL_USERNAME'] = os.environ.get('EMAIL_USER')
+app.config['MAIL_PASSWORD'] = os.environ.get('EMAIL_PASS')
+mail = Mail(app)
 
 from flaskblog import routes
